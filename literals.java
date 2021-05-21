5baef76e929a5e40a7d1cacddebe058af53de371
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-05-21 14:03:04 +0530

    Create literals.java

---
+++ literals.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import com.sun.tools.attach.AgentInitializationException;
+
+public class CWH_04_literals {
+    public static void main(String[] args) {
+        byte age=24;
+        int age1 =67;
+        short age2 =76;
+        long l =5666666666L;
+        char ch='D';
+        float f= 1.9999f;
+        double d =283.72728D;
+        boolean bool =true;
+        String str = "Divyanshu";
+
+        System.out.println(age);
+        System.out.println(age1);
+        System.out.println(age2);
+        System.out.println(l);
+        System.out.println(ch);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(bool);
+        System.out.println(str);
+
+    }
+}
