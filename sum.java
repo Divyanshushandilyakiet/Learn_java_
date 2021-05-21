65cfa1815ba4baf73d6cc53255dde8dd73c5ea98
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-05-21 10:54:58 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+      System.out.print("The sum of 4+5+6 is: ");
+      int num1=4;
+      int num2=5;
+      int num3=6;
+      int sum=num1+num2+num3;
+      System.out.println(sum);
+    }
+}
