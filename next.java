9f11ff3a342d26d98d23d7aefeb995374fe2d155
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-05-22 10:15:32 +0530

    Create next.java

---
+++ next.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+
+public class next {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a word");
+//            String str = sc.next();
+        System.out.println("Enter a line");
+        String str =sc.nextLine();
+        System.out.println(str);
+    }
+}
