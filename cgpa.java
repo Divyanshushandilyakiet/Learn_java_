e42ebd4dc80d8df544370ed57630103adb4ef186
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-05-31 10:14:21 +0530

    Create cgpa.java

---
+++ cgpa.java
@@ -0,0 +1,11 @@
+package com.company;
+//cgpa in three subjects
+public class cgpa {
+    public static void main(String[] args) {
+        float sub1=98;
+        float sub2=87;
+        float sub3=86;
+        float cgpa = (sub1+sub2+sub3)/3;
+        System.out.println("The cgpa is" +cgpa);
+    }
+}
