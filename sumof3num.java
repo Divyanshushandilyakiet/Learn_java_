0b90ef618bfb795ca14161ed82266ff361782bcb
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-05-31 10:13:38 +0530

    Create sumof3num.java

---
+++ sumof3num.java
@@ -0,0 +1,11 @@
+package com.company;
+//sum of three numbers
+public class sum {
+    public static void main(String[] args) {
+        float a=45;
+        float b=87;
+        float c=38;
+        float sum = a+b+c;
+        System.out.println("The sum of thre numbers is "+ sum );
+    }
+}
