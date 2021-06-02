43d64a1aeeb3f8feb15192c6c8731ed68517a189
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-06-02 22:14:55 +0530

    Create practice3.java

---
+++ practice3.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class practice3 {
+    public static void main(String[] args) {
+        System.out.println("Enter a number");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a > 8);
+    }
+}
