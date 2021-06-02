dc539d5234e1f956c138a4937061c25e1aaba6f7
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-06-02 22:06:54 +0530

    Create practice2.java

---
+++ practice2.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class practice2 {
+    public static void main(String[] args) {
+        char grade = 'B';
+        grade =(char)(grade+8);
+        System.out.println(grade);
+
+//        Decrypting the grade
+
+        grade =(char)(grade-8);
+        System.out.println(grade);
+
+    }
+}
