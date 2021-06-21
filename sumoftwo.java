052bbd46901794be661088b2c9ff0dd97c7638e6
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-06-21 23:54:08 +0530

    Create sumoftwo.java

---
+++ sumoftwo.java
@@ -0,0 +1,8 @@
+public class sum {
+    public static void main(String[] args) {
+        int a=3;
+        int b=7;
+        int c=a+b;
+        System.out.println("The sum of "+a+" and "+b+" is "+c);
+    }
+}
