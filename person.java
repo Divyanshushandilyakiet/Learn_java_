0db8f3dd3d41c22a790afec5c4df5ce27702e5e9
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-06-28 23:07:49 +0530

    Create person.java

---
+++ person.java
@@ -0,0 +1,20 @@
+package com.company;
+public class person {
+    public int age;
+    public String name;
+    public void setAge(int a){
+        age=a;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public int getAge()
+    {
+        return(age);
+    }
+
+    public String getName()
+    {
+        return (name);
+    }
+}
