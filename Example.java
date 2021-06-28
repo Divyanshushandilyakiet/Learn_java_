fdf92a6fad001c9bad55d8d25166667f61bfffbb
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-06-28 23:09:01 +0530

    Create Example.java

---
+++ Example.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class example {
+    public static void main(String[] args) {
+        student s1=new student();
+        s1.setRollno(100);
+        s1.setName("Divyanshu");
+        s1.setAge(24);
+        System.out.println("Roll no: "+s1.getRollno());
+        System.out.println("Name: "+s1.getName());
+        System.out.println("Age: "+s1.getAge());
+
+    }
+}
