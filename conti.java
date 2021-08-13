abfecaa5d40ad1270eca7cb924c4fea56e677423
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-08-14 01:28:00 +0530

    Create conti.java

---
+++ conti.java
@@ -0,0 +1,24 @@
+//use of continue in java 
+
+package corejava;
+
+public class conti {
+	public static void main(String args[])
+	{
+		// outer loop
+		for(int i=1;i<=3;i++)
+		{
+			// inner loop
+			
+			for(int j=1;j<=3;j++) {
+				
+				if(i==2&&j==2) {
+					continue;
+				}
+			
+			System.out.println(i+" "+j);
+			}
+		}
+	}
+
+}
