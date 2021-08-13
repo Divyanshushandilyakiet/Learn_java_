0cc9149b84eb649bf58f5e986709496cd8562b1e
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-08-13 23:47:13 +0530

    Create break.java

---
+++ break.java
@@ -0,0 +1,15 @@
+//use of break keyword
+package corejava;
+
+public class br {
+public static void main(String args[]) {
+
+ for(int i=1;i<=10;i++) {
+	 if(i==6)
+	 {
+		 break;
+	 }
+	 System.out.println(i);
+ }
+ }
+}
