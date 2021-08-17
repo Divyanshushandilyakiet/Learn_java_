21e2c1c3235fc13a0c9bbefc8dbbc2690120beb3
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-08-17 16:51:02 +0530

    Create Students.java

---
+++ Students.java
@@ -0,0 +1,17 @@
+//Encapsulation
+package corejava;
+
+public class Students {
+	
+	private String name;
+	 
+	public String getname() {
+		return name;
+	}
+	public void setname(String name) {
+		
+		this.name=name;
+		
+	}
+
+}
