1c95d488377d86346de71771a0115344400777b6
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-08-17 16:52:04 +0530

    Create Tests.java

---
+++ Tests.java
@@ -0,0 +1,14 @@
+//Encapsulation import
+
+package javatpoint;
+
+import corejava.Students;
+
+public class Tests {
+	public static void main(String[] args) {
+		Students s=new Students();
+		s.setname("Divyanshu");
+		System.out.println(s.getname());
+		
+	}
+}
