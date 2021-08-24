2a33192a5bfff16b5e463b1e2f8c05e316b189e5
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-08-24 22:25:24 +0530

    Create Testarr.java

---
+++ Testarr.java
@@ -0,0 +1,12 @@
+//Array
+package javatpoint;
+
+public class Testarr {
+public static void main(String[] args) {
+	int arr[]= {23,243,221,654,213,123};
+	for(int i=0;i<arr.length;i++)
+	{
+	   System.out.println(arr[i]);
+	}
+}
+}
