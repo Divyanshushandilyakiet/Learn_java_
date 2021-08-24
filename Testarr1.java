ed00087097e1533973a476ff5c29e342121871b3
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-08-24 22:50:50 +0530

    Create Testarr1.java

---
+++ Testarr1.java
@@ -0,0 +1,13 @@
+//Use of for each loop in array
+package javatpoint;
+
+public class Testarr1 {
+	public static void main(String[] args) {
+		int arr[]= {123,324,456,234,123,213};
+		for(int a:arr)
+		{
+			System.out.println(a);
+		}
+	}
+
+}
