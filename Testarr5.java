1700461ae0ed7bb63b613117438c006d3d404035
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-08-26 22:43:33 +0530

    Create  Testarr5.java

---
+++ Testarr5.java
@@ -0,0 +1,16 @@
+// Multidimensional array
+package javatpoint;
+
+public class Testarr5 {
+ public static void main(String[] args) {
+	int arr[][]= {{1,2,3},{4,5,6}};
+	for(int i=0;i<3;i++) {
+	for(int j=0;j<3;j++)
+	{
+		System.out.println(arr[i][j]+" ");
+	}
+	System.out.println();
+	}	
+ }
+ 
+}
