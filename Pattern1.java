04fbd4e47354332dcb5cca1b9bcef79b8e51c609
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-09-17 22:45:35 +0530

    Create  Pattern1.java

---
+++ Pattern1.java
@@ -0,0 +1,15 @@
+package basicProgram;
+
+public class Pattern1 {
+	public static void main(String[] args) {
+		for(int i=0;i<6;i++)
+		   {
+			for(int j=0;j<=i;j++)
+			 {
+			System.out.print("*");
+			 }
+			  System.out.println(); 
+		   }	 
+	}
+
+}
