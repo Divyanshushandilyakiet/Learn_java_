ff9b4c636df6ad80c68d0bf77e8214106b781e2a
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-09-18 23:35:51 +0530

    Create Multiii.java

---
+++ Multiii.java
@@ -0,0 +1,11 @@
+package basicProgram;
+
+public class Multiii {
+public static void main(String[] args) {
+	float a= 25.5f;
+	float b= 3.5f;
+	float c= 40.5f;
+	float d=4.5f;
+	System.out.println((a*b-b*b)/(c-d));
+}
+}
