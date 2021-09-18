114f04a370c572909fb3f8d0644efbd821ec651c
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-09-18 23:36:54 +0530

    Create Table.java

---
+++ Table.java
@@ -0,0 +1,15 @@
+package basicProgram;
+
+import java.util.Scanner;
+
+public class Table {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(a+"*"+i+"="+a*i);
+		}
+	}
+
+}
