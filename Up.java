0bfcabb6f49111c90707d3d3aac13c7995b370d8
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-12-10 08:57:33 +0530

    Create Up.java

---
+++ Up.java
@@ -0,0 +1,23 @@
+package practice;
+
+public class Up extends UK {
+	   public void up() {
+		   System.out.println("Uttar ppradesh");
+		   Dis1();
+		   Dis2();
+		   Dis3();
+		   Dis4();
+	   }
+	   private void Dis1() {
+		   System.out.println("Ghaziabad");
+	   }
+	   private void Dis2() {
+		   System.out.println("Meerut");
+	   }
+	   private void Dis3() {
+		   System.out.println("Gorakhpur");
+	   }
+	   private void Dis4() {
+		   System.out.println("Hapur");
+	   }
+	}
