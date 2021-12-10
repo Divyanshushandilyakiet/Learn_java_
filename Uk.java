206a4f0a567af266e6e9529eecee3ae58bb5a828
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-12-10 08:57:07 +0530

    Create Uk.java

---
+++ Uk.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class UK {
+	public void uk() {
+		   System.out.println("Uttrakhand");
+		   Dis1();
+		   Dis2();
+	   }
+	   private void Dis1() {
+		   System.out.println("Haridwar");
+	   }
+	   private void Dis2() {
+		   System.out.println("Chamoli");
+	   }
+}
