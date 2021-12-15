af6bafb86792f77ae2edf36c1eb72118a742e686
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-12-15 10:28:55 +0530

    Update Str1.java

--- Str1.java
+++ Str1.java
@@ -4,9 +4,19 @@ public class Str1 {
 public static void main(String[] args) {
 	String s3= "india";
 	String s4= "INDia";
-//	System.out.println(s3.toUpperCase());
-//	System.out.println(s4.toLowerCase());
-//	System.out.println(s3.substring(2).toUpperCase());
+	
+	System.out.println(s3.toUpperCase());
+	
+	System.out.println(s4.toLowerCase());
+	
+	System.out.println(s3.substring(2).toUpperCase());
+	
 	System.out.println(s3.substring(0, 1)+s3.substring(3, 4));
+	
+//	change the string to character 
+        char[] ch=s3.toCharArray();
+        for(char p :ch) {
+    	System.out.println(p);
+        }
 }
 }
