9bc49b40da9948ca952dc977fe74567ed286d9ac
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-12-15 09:38:12 +0530

    Create  Str1.java

---
+++ Str1.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class Str1 {
+public static void main(String[] args) {
+	String s3= "india";
+	String s4= "INDia";
+	System.out.println(s3.toUpperCase());
+	System.out.println(s4.toLowerCase());
+}
+}
