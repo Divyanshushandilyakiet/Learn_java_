eff851b969759e684c56da96eb90775a7e8a8dbb
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2022-03-14 22:32:01 +0530

    Create Collect.java

---
+++ Collect.java
@@ -0,0 +1,17 @@
+package Practice;
+//collection arraylist
+import java.util.ArrayList;
+import java.util.List;
+
+public class Collect {
+	public List<String> getList() {
+		List<String> l=new ArrayList<String>();
+		l.add("Divyanshu");
+		l.add("Nishant");
+		l.add("Abhijeet");
+		l.add("Akshay");
+		return l;
+		
+	}
+
+}
