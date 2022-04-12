345681b1ae5658cef005e846986c03fd2e856286
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2022-04-12 10:04:14 +0530

    Create  Emp.java

---
+++ Emp.java
@@ -0,0 +1,26 @@
+package Practice;
+//ArrayList collection
+public class Emp {
+  private int id;
+  private String name;
+  private String add;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getAdd() {
+	return add;
+}
+public void setAdd(String add) {
+	this.add = add;
+}
+   
+}
